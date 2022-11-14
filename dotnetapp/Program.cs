d30d9055cd794874c85103652923d031c243415b
jithin <jitkv20@gmail.com>
2022-11-14 17:18:39 -0500

    made changes

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using k8s;
+using System.IO;
 
 namespace dotnetapp // Note: actual namespace depends on the project name.
 {
@@ -7,10 +8,16 @@ namespace dotnetapp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
+            DirectoryInfo files= new DirectoryInfo("/config");
+            Console.WriteLine("File details:");
+            foreach(FileInfo fi in files.GetFiles()){
+                Console.WriteLine(fi.FullName);
+            }
 
+            Console.WriteLine(" doing k8s stuff");
             // Load from in-cluster configuration:
             var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(Environment.GetEnvironmentVariable("KUBECONFIG"));
-            
+
             var client = new Kubernetes(config);
 
             k8s.Models.V1PodList plist = client.CoreV1.ListNamespacedPod("xmrig");
