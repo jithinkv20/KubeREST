505a2c3416fe6701591fc8a26012cfaf4ec9c132
jithin <jitkv20@gmail.com>
2022-11-14 11:00:35 -0500

    first commit

---
+++ dotnetapp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using k8s;
+
+namespace dotnetapp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // Load from in-cluster configuration:
+            var config = KubernetesClientConfiguration.InClusterConfig();
+
+            var client = new Kubernetes(config);
+
+            k8s.Models.V1PodList plist = client.CoreV1.ListNamespacedPod("xmrig");
+            foreach(var items in plist.Items){
+                Console.WriteLine(items.Metadata.Name);
+            }
+        }
+    }
+}
