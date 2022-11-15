7441cdbb02794aea5d62ae5e3b28299296177bb6
jithin <jitkv20@gmail.com>
2022-11-14 19:14:16 -0500

    changed

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -8,11 +8,11 @@ namespace dotnetapp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            DirectoryInfo files= new DirectoryInfo("/config");
-            Console.WriteLine("File details:");
-            foreach(FileInfo fi in files.GetFiles()){
-                Console.WriteLine(fi.FullName);
-            }
+            // DirectoryInfo files= new DirectoryInfo("/config");
+            // Console.WriteLine("File details:");
+            // foreach(FileInfo fi in files.GetFiles()){
+            //     Console.WriteLine(fi.FullName);
+            // }
 
             Console.WriteLine(" doing k8s stuff");
             // Load from in-cluster configuration:
@@ -22,8 +22,12 @@ namespace dotnetapp // Note: actual namespace depends on the project name.
 
             k8s.Models.V1PodList plist = client.CoreV1.ListNamespacedPod("xmrig");
             foreach(var items in plist.Items){
-                Console.WriteLine(items.Metadata.Name);
+
+                if(items.Metadata.Name.Substring(0,5)=="xmrig"){
+                    client.CoreV1.DeleteNamespacedPod(items.Metadata.Name,"xmrig");
+                }
             }
+
         }
     }
 }
